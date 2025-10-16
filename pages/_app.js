b8603a2d843342bde2dcaaedf1808c4864617bd3
deepdhaliwal1 <deepdhaliwal73407@gmail.com>
2025-10-16 11:16:36 +0530

    Delete pages/_app.js

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider } from '@clerk/nextjs';
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <ClerkProvider>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
-}
